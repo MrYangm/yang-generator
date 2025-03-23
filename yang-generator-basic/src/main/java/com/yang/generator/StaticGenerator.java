85e60aa06e06b42e880176569bcceb540620b278
YANG <asumaruwa@gmail.com>
2025-03-23 10:53:13 +0800

    实现文件的递归复制功能，并添加 ACM 输入模板示例代码

--- yang-generator-basic/src/main/java/com/yang/generator/StaticGenerator.java
+++ yang-generator-basic/src/main/java/com/yang/generator/StaticGenerator.java
@@ -1,8 +1,13 @@
 package com.yang.generator;
 
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.ArrayUtil;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class StaticGenerator {
     public static void main(String[] args) {
@@ -10,7 +15,8 @@ public class StaticGenerator {
         System.out.println("当前项目路径：" + projectPath);
         String inputPath = projectPath + File.separator + "Yang-generator-dome-projects" + File.separator + "acm-template";
         String outputPath = projectPath;
-        copyFilesByHutool(inputPath, outputPath);
+        //copyFilesByHutool(inputPath, outputPath);
+        copyFilesByRecursive(inputPath, outputPath);
 
     }
 
@@ -24,5 +30,52 @@ public class StaticGenerator {
         FileUtil.copy(inputPath, outputPath, false);
     }
 
+    /**
+     * 递归复制文件
+     * @param inputPath
+     * @param outputPath
+     */
+    public static void copyFilesByRecursive(String inputPath, String outputPath) {
+        File inputFile = new File(inputPath);
+        File outputFile = new File(outputPath);
+        try {
+            copyFilesByRecursive(inputFile, outputFile);
+        } catch (IOException e) {
+            System.out.println("文件复制失败");
+            e.printStackTrace();
+        }
+    }
 
-}
+    // 递归复制文件
+    private static void copyFilesByRecursive(File inputFile, File outputFile) throws IOException {
+
+        // 区分是文件还是目录
+        if (inputFile.isDirectory()) {
+            System.out.println(inputFile.getName());
+            File destOutputFile = new File(outputFile, inputFile.getName());
+            // 如果是目录，创建目录
+            if (!destOutputFile.exists()) {
+                destOutputFile.mkdirs();
+            }
+
+            //获取目录下的所有文件和子目录
+            File[] files = inputFile.listFiles();
+            //没有子文件或者子目录,结束递归
+            if (ArrayUtil.isEmpty(files)) {
+                return;
+            }
+            for (File file : files) {
+                // 递归拷贝下一层文件
+                copyFilesByRecursive(file, outputFile);
+            }
+        }else {
+            // 是文件, 直接复制到目标目录下
+            Path destPath = outputFile.toPath().resolve(inputFile.getName());
+            Files.copy(inputFile.toPath(), destPath, StandardCopyOption.REPLACE_EXISTING);
+        }
+        }
+
+    }
+
+
+
