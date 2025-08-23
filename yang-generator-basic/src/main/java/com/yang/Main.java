5531607bed0a9398868bec66c9d3867c71fdf8b9
yang <asumaruwa@gmail.com>
2025-08-23 15:06:19 +0800

    add initial project files including Main, MainTemplate, StaticGenerator, and pom.xml

---
+++ yang-generator-basic/src/main/java/com/yang/Main.java
@@ -0,0 +1,12 @@
+package com.yang;
+
+/**
+ * @Author: yang
+ * @Date: 2025/08/23/12:41
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, YANG!");
+    }
+}
